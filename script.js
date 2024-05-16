500c63f1ac4b066135e09c0d1b625048875c4659
Itachi-789 <vighneshzagade4@gmail.com>
2024-05-16 11:13:08 +0530

    Implementing-game-logic

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function(message){
+    document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    displayMessage('⛔ No number!')
+  } else if (guess === secretNumber) {
+    displayMessage('😁 Correct Guess')
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+    displayMessage((guess > secretNumber) ? '😟 High' : '😟 Low')
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.title').textContent = 'Game Over!!';
+      document.querySelector('.score').textContent = 0;
+      displayMessage('👎👎')
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('body').style.backgroundColor = '#222';
+  displayMessage('Guess the Number🤔')
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
